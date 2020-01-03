9e5c975e0a03099320ad72758d85f35fa9893a1c
keiya01 <keiya.s.0210@gmail.com>
2020-01-03 16:35:46 +0900

    Add function to restart when worker was clushed

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,34 @@ if(cluster.isMaster) {
   for(let i = 0; i < cpus; i++) {
     cluster.fork();
   }
-} else {
+
+  cluster.on("exit", (worker, code) => {
+    if(code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log("Worker was restarted");
+      cluster.fork();
+    }
+  });
+
+  process.on("SIGUSR2", () => {
+    const workers = Object.keys(cluster.workers);
+    const restartWorker = (i: number) => {
+      if(i >= workers.length) return;
+
+      const worker = cluster.workers[workers[i]];
+      if(!worker) return;
+
+      console.log(`Stopping worker is ${worker.process.pid}`);
+      worker.disconnect();
+
+      worker.on("disconnect", () => {
+        const newWorker = cluster.fork();
+        newWorker.on("listening", () => {
+          restartWorker(i + 1);
+        });
+      });
+    }
+    restartWorker(0);
+  });
+} else if(cluster.isWorker) {
   require("./server");
 }
