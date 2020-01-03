b3330c4c28de74f85c0397c39232e60a5da1ed44
keiya01 <keiya.s.0210@gmail.com>
2020-01-03 17:07:13 +0900

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import cluster from "cluster";
 import os from "os";
 
+// マスタープロセスの時のみ実行される
 if(cluster.isMaster) {
   const cpus = os.cpus().length;
   console.log(`Clustering to ${cpus} cpus`);
+
+  // マスタープロセスから CPU の数だけ新しいインスタンスを fork する
   for(let i = 0; i < cpus; i++) {
     cluster.fork();
   }
 
+  // エラーが発生して worker が exit したときに新しいインスタンスを fork する
   cluster.on("exit", (worker, code, signal) => {
     if(code !== 0 && !worker.exitedAfterDisconnect) {
       console.log("Worker was restarted: ", worker.process.pid, code, signal);
@@ -15,6 +19,11 @@ if(cluster.isMaster) {
     }
   });
 
+  /** 
+   * ユーザー定義の signal を受け取る
+   * ここでは SIGUSR2 を受け取ったときにそれぞれの worker をリスタートするように
+   * 実装している(サーバーをアップデートするときなどにダウンタイムなくリスタートするため)
+  */
   process.on("SIGUSR2", () => {
     const workers = Object.keys(cluster.workers);
     const restartWorker = (i: number) => {
@@ -36,5 +45,9 @@ if(cluster.isMaster) {
     restartWorker(0);
   });
 } else if(cluster.isWorker) {
+  /**
+   * マスタープロセスでの処理が終わるとワーカープロセスに切り替わる
+   * ワーカープロセスでサーバーを起動させる
+  */
   require("./server");
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import http from "http";
 const pid = process.pid;
 
 const server = http.createServer((_, res) => {
+  // 重い処理を再現するためにループ処理を実行している
   for(let i = 1e7; i > 0; i--) {}
   res.end(`Recieved request from ${pid}`);
 });
@@ -11,6 +12,7 @@ server.listen(process.argv[2] || 3000, () => {
   console.log(`Started in ${pid}`);
 });
 
+// exit イベントを発生させるためにランダムな時間でエラーを発生させている
 setTimeout(() => {
   throw new Error("Worker clashed");
 }, Math.ceil(Math.random() * 100) * 1000);
