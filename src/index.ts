8c063c066047f81ecb29b01b5b2bc64aeb753951
keiya01 <keiya.s.0210@gmail.com>
2020-01-03 13:41:30 +0900

    Add cluster module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const add = (n: number) => {
-  return n * n;
+import cluster from "cluster";
+import os from "os";
+
+if(cluster.isMaster) {
+  const cpus = os.cpus().length;
+  console.log(`Clustering to ${cpus} cpus`);
+  for(let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  require("./server");
 }
